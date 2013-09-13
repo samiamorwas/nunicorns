1c5b5a8f4e7e79f046268059682d5f4c994dc796
samiamorwas <samiamorwas@gmail.com>
2013-09-13 14:26:03 -0400

    Fixed the brand logo not linking to the most recent post.

--- js/router.js
+++ js/router.js
@@ -4,9 +4,18 @@ App.Router.map(function() {
 	});
 });
 
+/*
+	This is some funky junk, brother. We set the model for
+	the index route to be the most recent post, so that
+	going to the site will show that first. Then we pass that
+	to the application controller, so that the brand logo
+	will link to the right post. There seems to be no good
+	way to get it to link to the index and show the latest
+	post, because linkTo won't call the model hook for
+	dynamic urls.
+*/
 App.PostsIndexRoute = Ember.Route.extend({
 	model: function() {
-		// This doesn't seem to be working when transitioning back to the index
 		return this.store.findAll('post').then(function(posts) {
 			return posts.get('lastObject');
 		});
@@ -14,17 +23,16 @@ App.PostsIndexRoute = Ember.Route.extend({
 
 	renderTemplate: function() {
 		this.render('post');
+	},
+
+	setupController: function(controller, model) {
+		controller.set('model', model);
+		this.controllerFor('application').set('recentPost', model);
 	}
 });
 
 App.PostsRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.findAll('post');
-	},
-
-	setupController: function(controller, model) {
-		controller.set('model', model);
-		controller.set('firstPost', 0);
-		controller.set('lastPost', 1);
 	}
 });
